a0b1b9a1520e0d04008218693a5cfd8eebc96548
Polina <sgsey75@gmail.com>
2021-07-12 00:24:24 +0400

    Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,48 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = b**2 - ((4 * a) * c);
+    
+  if (D > 0) {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr = [x1, x2];
+  }
+  else if (D === 0) {
+    let x = -b / (2 * a);
+    arr = [x];
+  } 
+  
+  return arr; 
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    if(typeof percent !== "number") {
+      return  'Параметр "Процентная ставка" содержит неправильное значение "' + percent + '"';
+    }
 
-  // код для задачи №2 писать здесь
+    if(typeof contribution !== "number") {
+      return  'Параметр "Начальный взнос" содержит неправильное значение "' + contribution + '"';
+    }
+      
+    if(typeof amount !== "number") {
+      return  'Параметр "Общая стоимость" содержит неправильное значение "' + amount + '"';
+    }
 
-  return totalAmount;
+    let bodyCredit = amount - contribution;
+    let numberOfMonths = monthDiff(new Date(), date);
+    let percentOfMonth = percent / 12 / 100;
+    let payment = bodyCredit * ((percentOfMonth + percentOfMonth / ((Math.pow(1 + percentOfMonth, numberOfMonths) - 1))));
+    let totalAmount = payment * numberOfMonths;
+
+    console.log(totalAmount.toFixed(2));
+    return parseFloat(totalAmount.toFixed(2));
+}
+
+function monthDiff(d1, d2) {
+  let months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
 }
