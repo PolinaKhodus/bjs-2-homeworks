ee0c6aad6ad5d41afd0fc02b7f004fefe2df63e4
Polina <sgsey75@gmail.com>
2021-07-20 01:44:46 +0400

    resolve task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+   
+ Student.prototype.setSubject = function (subjectName) {
+ this.subject = subjectName;
+}
+  
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+    console.log(this.marks)
+} 
 
+Student.prototype.addMarks = function (...marks) {
+  this.marks = marks;
+ }
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for( let i = 0; i < this.marks.length; i += 1) {
+    sum += this.marks[i];
+  }
+  return sum / this.marks.length;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
-
-// ваш код для остальных методов
